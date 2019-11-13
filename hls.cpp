cc34c3799083d07f985a932d1c51a373552536b9
hoyong.eom <hoyo1744@naver.com>
2019-11-14 00:38:46 +0900

    2

--- hls.cpp
+++ hls.cpp
@@ -1,60 +1,204 @@
 #include "hls_define.h"
 
+enum FILETYPE{
+	NONE=-1,
+	EXE_FILE=0,
+	ZIP_FILE=1,
+	REG_FILE=2,
+	FIFO_FILE=3,
+	CHR_FILE=4,
+	DIR_FILE=5,
+	BLK_FILE=6,
+	LNK_FILE=7,
+	SOCK_FILE=8,
+	WHT_FILE=9
+};
+
 char pwd[FILE_BUFF];
 DIR* dir=NULL;
 struct dirent* ent=NULL;
 FILETYPE fileType=NONE;
-enum FILETYPE
-{
-	NONE=-1;
-	EXE_FILE=0;
-	ZIP_FILE=1;
-	REG_FILE=2;
-	FIFO_FILE=3;
-	DIR_FILE=4;
-	BLK_FILE=5;
-	LNK_FILE=6;
-	SOCK_FILE=7;
-	WHT_FILE=8;
-};
-
+int color=-1;
 void Init()
 {
+	color=-1;
 	fileType=NONE;
 	dir=NULL;
 	ent=NULL;
 	memset(pwd,0,sizeof(pwd));
+}
+
+void setColor(int color)
+{
+	printf("\033[1;%dm",color);
+}
+void setColorInit()
+{
+	printf("\033[1;0m");
+}
+mode_t getFileMode(char* name)
+{
+	struct stat* buf;
+	string path=pwd;
+	path+="/";
+	path+=name;
+
+	int ret=stat(path.c_str(),buf);
+	if(ret!=0)
+	{
+		perror("stat");
+		exit(1);
+	}
+
+	return buf->st_mode;
+
+}
+bool IsExcute(mode_t md)
+{
+	if(md==S_IRWXU || md==S_IXUSR || md==S_IRWXG || md==S_IXGRP || md==S_IRWXO || md==S_IXOTH)
+		return true;
+}
+bool IsCompressed(char *name)
+{
+	if(strstr(name,".tar"))
+		return true;
+	if(strstr(name,".tgz"))
+		return true;
+	if(strstr(name,".arc"))
+		return true;
+	if(strstr(name,".arj"))
+		return true;
+	if(strstr(name,".taz"))
+		return true;
+	if(strstr(name,".lha"))
+		return true;
+	if(strstr(name,".lz4"))
+		return true;
+	if(strstr(name,".lzh"))
+		return true;
+	if(strstr(name,".lzma"))
+		return true;	
+	if(strstr(name,".tlz"))
+		return true;
+	if(strstr(name,".txz"))
+		return true;
+	if(strstr(name,".tzo"))
+		return true;
+	if(strstr(name,".t7z"))
+		return true;
+	if(strstr(name,".zip"))
+		return true;
+	if(strstr(name,".z"))
+		return true;
+	if(strstr(name,".Z"))
+		return true;
+	if(strstr(name,".dz"))
+		return true;
+	if(strstr(name,".gz"))
+		return true;
+	if(strstr(name,".lrz"))
+		return true;
+	if(strstr(name,".lz"))
+		return true;
+	if(strstr(name,".lzo"))
+		return true;
+	if(strstr(name,".xz"))
+		return true;
+	if(strstr(name,".bz2"))
+		return true;
+	if(strstr(name,".bz"))
+		return true;
+	if(strstr(name,".tbz"))
+		return true;
+	if(strstr(name,".tbz2"))
+		return true;
+	if(strstr(name,".tz"))
+		return true;
+	if(strstr(name,".deb"))
+		return true;
+	if(strstr(name,".rpm"))
+		return true;
+	if(strstr(name,".jar"))
+		return true;
+	if(strstr(name,".war"))
+		return true;
+	if(strstr(name,".ear"))
+		return true;
+}
+
+bool IsImage(char* name)
+{
+
+	if(strstr(name,".jpg"))
+		return true;
+	if(strstr(name,".jpeg"))
+		return true;
+	if(strstr(name,".gif"))
+		return true;
+	if(strstr(name,".bmp"))
+		return true;
+	if(strstr(name,".pbm"))
+		return true;
+	if(strstr(name,".pgm"))
+		return true;
+	if(strstr(name,".ppm"))
+		return true;
+	if(strstr(name,".tga"))
+		return true;
+	if(strstr(name,".xbm"))
+		return true;
 }
 void checkFileType(int type,char* name)
 {
 	//1단계 : 파일타입에 따른 구분
 	//2단계 : 일반 파일일 경우 실행파일, 일반파일,압축파일 구분
 	
+	mode_t md=0;
 	switch(type)
 	{
 		case DT_UNKNOWN:
-			
+			//일반파일잠시보류
+			md=getFileMode(name);
+			if(IsExcute(md))
+				color=32;
+			else if(IsCompressed(name))
+				color=31;
+			else if(IsImage(name))
+				color=35;
+			else
+				color=30;
 			break;
 		case DT_FIFO:
-			fileType=FIFO_FILE
+			fileType=FIFO_FILE;
+			color=33;//초록
 			break;
 		case DT_DIR:
 			fileType=DIR_FILE;
+			color=34;//파랑
 			break;
 		case DT_BLK:
 			fileType=BLK_FILE;
+			color=33;//노랑
 			break;
 		case DT_LNK:
 			fileType=LNK_FILE;
+			color=36;//연두
 			break;
 		case DT_WHT:
 			fileType=WHT_FILE;
+			break;
+		case DT_SOCK:
+			fileType=SOCK_FILE;
+			color=35;//빨강
+			break;
+		case DT_CHR:
+			fileType=CHR_FILE;
+			color=33;//노랑
 			break;
 
 
 	}
 
-	
 
 }
 int main(void)
@@ -101,8 +245,11 @@ int main(void)
 			}
 		}
 		checkFileType(ent->d_type,ent->d_name);
-		printf("%s\n",ent->d_name);
+		
 
+		setColor(color);
+		printf("%s\n",ent->d_name);
+		setColorInit();
 	}
 
 	//디렉터리스트림해제
